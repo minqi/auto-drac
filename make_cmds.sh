59a25bbabd51946d7a645db9c5d59071b73b006d
minqi <minqijiang@fb.com>
2020-11-05 12:54:25 +0000

    add make_cmds.sh

--- make_cmds.sh
+++ make_cmds.sh
@@ -1,16 +1,21 @@
 num_trials=5
 seeds=(0 1 2 3 4)
 envs=(
+	"bigfish"
 	"bossfight"
 	"caveflyer"
+	"chaser"
 	"climber"
 	"coinrun"
+	"dodgeball"
 	"fruitbot"
 	"heist"
 	"jumper"
 	"leaper"
 	"maze"
+	"miner"
 	"ninja"
+	"plunder"
 	"starpilot"
 )
 
@@ -22,13 +27,13 @@ echo "python train.py \\
 --env_name=$env \\
 --use_ucb=True \\
 --num_processes=64 \\
---log_interval=10 \\
+--log_interval=1 \\
 --save_interval=10 \\
 --num_levels=200 \\
 --save_dir='~/logs/autodrac' \\
 --log_dir='~/logs/autodrac' \\
 --seed=$seed \\
---run_name=ucb-drac \\
+--run_name=ucb-drac-log1 \\
 --wandb_entity=level-replay \\
 --wandb_project=ucb-drac
 "
